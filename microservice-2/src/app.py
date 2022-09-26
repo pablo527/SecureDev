7f9fc708e49679c190bc6fa5b5c069a7aef4407e
PABLO <pablo_7v.m@hotmail.com>
2022-09-25 19:37:26 -0500

    ipv4 regex valided

--- microservice-2/src/app.py
+++ microservice-2/src/app.py
@@ -1,19 +1,26 @@
 from flask import Flask, jsonify, request
 from dbConnectClass import MongoDB
+import re
 
 app = Flask(__name__)
 
 @app.route('/insert-data', methods=['POST'])
 def ping():
     request_data = request.get_json()
-    print(request_data)
+    ip = request_data['block_ip']
     
-    mongoConnection = MongoDB();
-    mongoConnection.connetTo('my_db','ips')
+    ipv4_pattern = "^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    is_valid = re.match(ipv4_pattern, ip)
+
+    if is_valid:
+        mongoConnection = MongoDB();
+        mongoConnection.connetTo('my_db','ips')
+        mongoConnection.insertOneData(request_data)   
+        return jsonify({'reponse':'data insert'}) 
+
+    return jsonify({'reponse':'invalid'})     
+
     
-    mongoConnection.insertOneData(request_data)
-    
-    return jsonify({'reponse':'Data Insertada!'})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
